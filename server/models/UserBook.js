21cafc2ed51790c69e5863a57d9ca7e18febf9dd
timcoe429 <timcoe429@gmail.com>
2025-07-24 09:39:23 -0400

    Update UserBook.js

--- server/models/UserBook.js
+++ server/models/UserBook.js
@@ -1,1 +1,56 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../config/database');
+const User = require('./User');
+const Book = require('./Book');
 
+const UserBook = sequelize.define('UserBook', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: User,
+      key: 'id'
+    }
+  },
+  bookId: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: Book,
+      key: 'id'
+    }
+  },
+  status: {
+    type: DataTypes.ENUM('to-read', 'reading', 'read', 'dnf'),
+    defaultValue: 'to-read'
+  },
+  rating: {
+    type: DataTypes.INTEGER,
+    validate: { min: 1, max: 5 }
+  },
+  dateAdded: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  },
+  dateStarted: {
+    type: DataTypes.DATE
+  },
+  dateFinished: {
+    type: DataTypes.DATE
+  },
+  source: {
+    type: DataTypes.ENUM('goodreads', 'photo', 'manual'),
+    defaultValue: 'manual'
+  }
+});
+
+// Associations
+User.belongsToMany(Book, { through: UserBook });
+Book.belongsToMany(User, { through: UserBook });
+UserBook.belongsTo(User);
+UserBook.belongsTo(Book);
+
+module.exports = UserBook;
