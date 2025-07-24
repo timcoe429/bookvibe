1ba17783bc530960f17af005f5a24da6349d56b5
timcoe429 <timcoe429@gmail.com>
2025-07-24 09:39:04 -0400

    Update Book.js

--- server/models/Book.js
+++ server/models/Book.js
@@ -1,1 +1,57 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../config/database');
 
+const Book = sequelize.define('Book', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  author: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  isbn: {
+    type: DataTypes.STRING,
+    unique: true
+  },
+  goodreadsId: {
+    type: DataTypes.STRING,
+    unique: true
+  },
+  pages: {
+    type: DataTypes.INTEGER
+  },
+  description: {
+    type: DataTypes.TEXT
+  },
+  coverUrl: {
+    type: DataTypes.STRING
+  },
+  genre: {
+    type: DataTypes.STRING
+  },
+  mood: {
+    type: DataTypes.ENUM('escapist', 'intense', 'thoughtful', 'light'),
+    defaultValue: 'escapist'
+  },
+  averageRating: {
+    type: DataTypes.DECIMAL(3, 2)
+  },
+  publicationYear: {
+    type: DataTypes.INTEGER
+  }
+}, {
+  indexes: [
+    { fields: ['title'] },
+    { fields: ['author'] },
+    { fields: ['mood'] },
+    { fields: ['goodreadsId'] }
+  ]
+});
+
+module.exports = Book;
