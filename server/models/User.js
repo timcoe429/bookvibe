168a2561d7937948be26cebd45635f817cdcc061
timcoe429 <timcoe429@gmail.com>
2025-07-24 09:39:15 -0400

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,1 +1,38 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../config/database');
 
+const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  sessionId: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false
+  },
+  goodreadsUserId: {
+    type: DataTypes.STRING,
+    unique: true
+  },
+  preferences: {
+    type: DataTypes.JSONB,
+    defaultValue: {
+      favoriteGenres: [],
+      readingGoal: 52,
+      preferredMoods: ['escapist']
+    }
+  },
+  stats: {
+    type: DataTypes.JSONB,
+    defaultValue: {
+      booksThisYear: 0,
+      totalBooks: 0,
+      currentStreak: 0,
+      longestStreak: 0
+    }
+  }
+});
+
+module.exports = User;
